b7c6f679001a9096517195271b0d6a2d08318cb8
Polina Balova <polina77728@yandex.ru>
2022-11-26 12:54:58 +0300

    Задача 38 к семинару от 25.11.22г

---
+++ 38_task/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу 
+между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76 */
+
+int size = 10;
+double[] numbers = new double[size];
+double max = 0;
+double min = 0;
+double difference = 0;
+
+FillArrayRandomNumbers(numbers, -10, 10);
+Console.WriteLine();
+WriteArray(numbers);
+Console.WriteLine();
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];  
+    }
+
+   if (numbers[i] < min)
+    {
+        min = numbers[i];  
+    }
+}
+
+difference = max - min;
+Console.WriteLine($"Мах = {max} Min = {min}. Разница между мах и мin = {difference}");
+Console.WriteLine();
+
+void FillArrayRandomNumbers(double[] array, int min, int max)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100) + new Random().NextDouble();
+    }
+}
+
+void WriteArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
