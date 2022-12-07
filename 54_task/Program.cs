cab7c6c027e7c8c22fc950b311b89a462bbff178
Polina Balova <polina77728@yandex.ru>
2022-12-07 14:42:59 +0300

    Задача 54

---
+++ 54_task/Program.cs
@@ -0,0 +1,70 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2  */
+
+int rows = ReadInt("Введите количество строк матрицы: ");
+int columns = ReadInt("Введите количество столбцов матрицы: ");
+
+int[,] matrix = new int[rows, columns];
+FillMatrixRandomNumbers(matrix);
+WriteMatrix(matrix);
+
+
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int b = 0; b < matrix.GetLength(1); b++)
+        {
+            if (matrix[i, b] < matrix[i, j])
+            {
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[i, b];
+                matrix[i, b] = temp;
+            }
+        }
+    }
+} 
+
+
+
+
+WriteMatrix(matrix);
+
+void FillMatrixRandomNumbers(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void WriteMatrix(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
