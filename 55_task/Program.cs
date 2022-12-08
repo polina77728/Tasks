1795c1704bd5fc8704fd0244c12343156bb9cc07
Polina Balova <polina77728@yandex.ru>
2022-12-08 07:27:07 +0300

    Задача 55

---
+++ 55_task/Program.cs
@@ -0,0 +1,61 @@
+﻿//Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+//В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int rows = ReadInt("Введите количество строк матрицы: ");
+int columns = ReadInt("Введите количество столбцов матрицы: ");
+int[,] numbers = new int[rows, columns];
+
+FillMatrixRandomNumbers(numbers);
+WriteMatrix(numbers);
+
+if (rows == columns)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            if(i == j)
+                break;
+            else
+            {
+                int temp = numbers[i, j];
+                numbers[i, j] = numbers[j, i];
+                numbers[j, i] = temp;
+            }
+        }
+    }
+    WriteMatrix(numbers);
+}
+else
+    Console.WriteLine("Матрица не квадратная, невозможно заменить строки на столбцы");
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillMatrixRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+void WriteMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
