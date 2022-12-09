d0eca27072ba311b98a8b3801abeee5e6cb85905
Polina Balova <polina77728@yandex.ru>
2022-12-09 21:22:45 +0300

    Задача 60 3D

---
+++ 60_task_3Matr/Program.cs
@@ -0,0 +1,70 @@
+﻿/* Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся 
+двузначных чисел. Напишите программу, которая будет построчно выводить массив, 
+добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1) */
+
+int rows = ReadInt("Введите количество строк матрицы: ");
+int columns = ReadInt("Введите количество столбцов матрицы: ");
+int sheets = ReadInt("Введите количество листов матрицы: ");
+
+int[,,] Matrix3D = new int[rows, columns, sheets];
+
+Fill3DRandomNumbers(Matrix3D);
+PrintMatrix3D(Matrix3D);
+
+
+
+
+void Fill3DRandomNumbers(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i,j,k] = new Random().Next(10, 99);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintMatrix3D(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.WriteLine($"{matrix[i,j,k]} ([{i},{j},{k}])");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillMatrixRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
